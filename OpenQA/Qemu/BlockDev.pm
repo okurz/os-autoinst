d8c53f64eb20fdc2611f8259ea6a0d6efba3932f
Oliver Kurz <okurz@suse.de>
2021-06-21 20:13:54 +0200

    Use proper perl "signatures" in OpenQA/
    Using a command like
    
    ```
    sed -i "/^use strict;/ {N;s/use strict;/use Mojo::Base -strict, -signatures;/}" $(git ls-files)
    for i in $(git grep -l 'use \(strict\|warnings\)'); do grep -q 'use Mojo::Base -strict' $i && sed -i '/use \(strict\|warnings\)/d' $i; done
    tools/tidy --only-changed
    ```
    
    and some manual fixes.

--- OpenQA/Qemu/BlockDev.pm
+++ OpenQA/Qemu/BlockDev.pm
@@ -33,7 +33,7 @@ weakened.
 =cut
 
 package OpenQA::Qemu::BlockDev;
-use Mojo::Base 'OpenQA::Qemu::MutParams';
+use Mojo::Base 'OpenQA::Qemu::MutParams', -signatures;
 
 use Scalar::Util 'weaken';
 use OpenQA::Qemu::SnapshotConf;
@@ -87,9 +87,7 @@ still be taken from the backing files in the chain).
 To avoid memory leaks we weaken this reference.
 
 =cut
-sub overlay {
-    my ($self, $ol) = @_;
-
+sub overlay ($self, $ol) {
     return $self->{overlay} unless defined $ol;
     $self->{overlay} = $ol;
     weaken($self->{overlay});
@@ -128,8 +126,7 @@ any snapshot.
 has snapshot => sub { return OpenQA::Qemu::Snapshot->new() };
 
 # See MutParams.pm
-sub gen_cmdline {
-    my ($self) = @_;
+sub gen_cmdline ($self) {
     my @cmdl = ();
 
     # The first blockdev defines the data store, we only use files, but in
@@ -158,8 +155,7 @@ for the entire blockdevice chain or an empty array if it does not need
 creating.
 
 =cut
-sub gen_qemu_img_cmdlines {
-    my $self   = shift;
+sub gen_qemu_img_cmdlines ($self) {
     my @cmdlns = defined $self->backing_file ? $self->backing_file->gen_qemu_img_cmdlines : ();
     return @cmdlns unless $self->needs_creating;
 
@@ -178,18 +174,14 @@ If any image file in the chain is marked as needs_creating, but already exists
 this will return it in an array so that the caller can unlink it.
 
 =cut
-sub gen_unlink_list {
-    my $self = shift;
-
+sub gen_unlink_list ($self) {
     return () unless $self->needs_creating;
     return ($self->file, $self->backing_file->gen_unlink_list())
       if defined $self->backing_file;
     return ($self->file);
 }
 
-sub _to_map {
-    my $self = shift;
-
+sub _to_map ($self) {
     return {driver => $self->driver,
         file           => $self->file,
         node_name      => $self->node_name,
@@ -199,8 +191,7 @@ sub _to_map {
         snapshot       => $self->snapshot->sequence};
 }
 
-sub _from_map {
-    my ($self, $drives, $snap_conf) = @_;
+sub _from_map ($self, $drives, $snap_conf) {
     my ($this, @rest) = @$drives;
 
     $self->backing_file(OpenQA::Qemu::BlockDev->new()->_from_map(\@rest, $snap_conf))
