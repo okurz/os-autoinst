def43a3fa7f84b5eeeb2df7691cf0c1242876bc2
Oliver Kurz <okurz@suse.de>
2021-07-30 15:17:32 +0200

    Use trivial 'prove' call in wrapper Makefile to fully replace invoke-tests
    Motivation:
    Simplifying the build system by replacing the custom 'tools/invoke-tests'
    wrapper script with standard 'make test' and 'cover -test' calls. This
    aligns with common Perl practices and ensures better integration with
    CMake/Ninja while maintaining necessary test environment configurations and
    TAP output validation.
    
    Design Choices:
    - Introduced a standard 'test' target in the top-level Makefile that
      exports required environment variables (OS_AUTOINST_BUILD_DIRECTORY,
      PERL5LIB) and supports 'TESTS' and 'PROVE' overrides.
    - Configured the Makefile to use 'tools/prove_wrapper' as the default test
      runner to ensure unhandled test output is caught.
    - Standardized coverage artifact paths to the source directory in CMake to
      ensure correct Ninja dependency tracking and artifact visibility.
    - Re-implemented Devel::Cover ignore patterns and specific test environment
      options (like 'CoverageWorkaround') directly in the CMake and Makefile
      targets.
    - Migrated the 'test-local-author-perl' target in CMake to use the new
      'make test' infrastructure for 'xt/' tests.
    - Utilized 'cover -test' with a custom make command to trigger
      instrumented test runs using the standardized testing interface.
    
    Benefits:
    Eliminates the maintenance burden of a custom shell wrapper while providing
    a robust, standardized interface for testing and coverage analysis that
    works consistently across Make, CMake, and Ninja.
    
    Related issue: https://github.com/os-autoinst/os-autoinst/pull/1739

--- Makefile
+++ Makefile
@@ -21,7 +21,17 @@ Makefile: ;
 build/build.ninja:
 	@mkdir -p ${build}
 	@cmake -B ${build} -S . -G Ninja
-
 .PHONY: setup-hooks
 setup-hooks: ## Install pre-commit git hooks
 	pre-commit install --install-hooks -t commit-msg -t pre-commit
+
+# Devel::Cover works best with a simple "test" target in a top-level Makefile
+# Call "check" for all tests and checks
+TESTS ?= t/
+PROVE ?= tools/prove_wrapper
+.PHONY: test
+test: all
+	OS_AUTOINST_BUILD_DIRECTORY=. OS_AUTOINST_MAKE_TOOL=make \
+	PERL5LIB=".:ppmclibs/blib/lib:ppmclibs/blib/arch/auto/tinycv:${PERL5LIB}" \
+	$(if $(COVERAGE),PERL5OPT="-Iexternal/os-autoinst-common/lib -MTest::CheckGitStatus -It/lib -MCoverageWorkaround $(PERL5OPT)") \
+	${PROVE} -I. -r ${TESTS}
--- cmake/test-targets.cmake
+++ cmake/test-targets.cmake
@@ -152,41 +152,25 @@ endif ()
 
 # add targets for invoking Perl test suite
 find_program(PROVE_PATH prove_wrapper PATHS tools NO_DEFAULT_PATH)
-find_program(PROVE_PATH prove)
+find_program(PROVE_PATH prove REQUIRED)
 find_program(UNBUFFER_PATH unbuffer)
-if (PROVE_PATH)
-    set(INVOKE_TEST_ARGS --prove-tool "${PROVE_PATH}" --make-tool "${CMAKE_MAKE_PROGRAM}" --unbuffer-tool "${UNBUFFER_PATH}" --build-directory "${CMAKE_CURRENT_BINARY_DIR}")
-    add_test(
-        NAME test-perl-testsuite
-        COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/tools/invoke-tests" ${INVOKE_TEST_ARGS}
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-    )
-    set_tests_properties(test-perl-testsuite
-        PROPERTIES ENVIRONMENT "TESTS=t")
-    add_test(
-        NAME test-local-author-perl
-        COMMAND "${PROVE_PATH}" xt
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-    )
-else ()
-    message(STATUS "Set PROVE_PATH to the path of the prove executable to enable running the Perl testsuite.")
-endif ()
-
-# add build system targets for invoking specific tests
-add_custom_target(test-local-author-perl COMMAND ${CMAKE_CTEST_COMMAND} -R "test-local-author-perl" WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
-add_custom_target(test-local COMMAND ${CMAKE_CTEST_COMMAND} -R "test-local-.*" WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
-add_custom_target(test-doc COMMAND ${CMAKE_CTEST_COMMAND} -R "test-doc-.*" WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
-add_custom_target(test-installed-files COMMAND ${CMAKE_CTEST_COMMAND} -R "test-installed-files" WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
-if (PROVE_PATH)
-    add_custom_target(test-perl-testsuite
-        COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/tools/invoke-tests" ${INVOKE_TEST_ARGS}
-        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-        USES_TERMINAL
-    )
-endif ()
-add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} ${CMAKE_CTEST_ARGUMENTS} WORKING_DIRECTORY ${CMAKE_BINARY_DIR} USES_TERMINAL)
-add_custom_target(check-pkg-build COMMAND ${CMAKE_CTEST_COMMAND} ${CMAKE_CTEST_ARGUMENTS} -E "test-local-.*" WORKING_DIRECTORY ${CMAKE_BINARY_DIR} USES_TERMINAL)
-foreach (CUSTOM_TARGET test-perl-testsuite check check-pkg-build)
+add_test(
+    NAME test-local-author-perl
+    COMMAND make test TESTS="xt/" PROVE=${PROVE_PATH}
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+add_custom_target(test-local-author-perl COMMAND ${CMAKE_CTEST_COMMAND} -R "test-local-author-perl" USES_TERMINAL)
+add_custom_target(test-local COMMAND ${CMAKE_CTEST_COMMAND} -R "test-local-.*")
+add_custom_target(test-doc COMMAND ${CMAKE_CTEST_COMMAND} -R "test-doc-.*")
+add_custom_target(test-installed-files COMMAND ${CMAKE_CTEST_COMMAND} -R "test-installed-files")
+add_custom_target(test-perl-testsuite
+    COMMAND make test PROVE=${PROVE_PATH}
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    USES_TERMINAL
+)
+add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} USES_TERMINAL)
+add_custom_target(check-pkg-build COMMAND ${CMAKE_CTEST_COMMAND} -E "test-local-.*" USES_TERMINAL)
+foreach (CUSTOM_TARGET test-local-author-perl test-perl-testsuite check check-pkg-build)
     add_dependencies(${CUSTOM_TARGET} symlinks)
 endforeach ()
 
@@ -195,40 +179,41 @@ find_program(COVER_PATH cover)
 if (COVER_PATH AND PROVE_PATH)
     add_custom_command(
         COMMENT "Run Perl testsuite with coverage instrumentation if no coverage data has been collected so far"
-        COMMAND "${CMAKE_CURRENT_SOURCE_DIR}/tools/invoke-tests" --coverage --skip-if-cover-db-exists ${INVOKE_TEST_ARGS}
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
+        COMMAND "${COVER_PATH}" -test -make "make test COVERAGE=1 PROVE=${PROVE_PATH}" -ignore "^external/" -ignore "^tools/" -ignore "^t/data/" -ignore "^/tmp"
+        OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/cover_db"
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     )
     add_custom_command(
         COMMENT "Generate coverage report (HTML)"
-        COMMAND "${COVER_PATH}" -report html_basic "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
-        OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/coverage.html"
+        COMMAND "${COVER_PATH}" -report html_basic "${CMAKE_CURRENT_SOURCE_DIR}/cover_db" -outputdir "${CMAKE_CURRENT_SOURCE_DIR}"
+        DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/cover_db"
+        OUTPUT "${CMAKE_CURRENT_SOURCE_DIR}/coverage.html"
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     )
     add_custom_target(
         coverage-reset
         COMMENT "Resetting previously gathered Perl test suite coverage"
-        COMMAND rm -r "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
+        COMMAND rm -r "${CMAKE_CURRENT_SOURCE_DIR}/cover_db"
     )
     add_custom_target(
         coverage
         COMMENT "Perl test suite coverage (HTML)"
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/coverage.html"
+        DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/coverage.html"
     )
     add_dependencies(coverage symlinks)
     add_custom_target(
         coverage-codecov
         COMMENT "Perl test suite coverage (codecov, if direct report uploading possible, e.g. within travis CI)"
-        COMMAND "${COVER_PATH}" -report codecov "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
+        COMMAND "${COVER_PATH}" -report codecov
+        DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/cover_db"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     )
     add_dependencies(coverage-codecov symlinks)
     add_custom_target(
         coverage-codecovbash
         COMMENT "Perl test suite coverage (codecovbash, useful if direct report upload not available)"
-        COMMAND "${COVER_PATH}" -report codecovbash "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/cover_db"
+        COMMAND "${COVER_PATH}" -report codecovbash "${CMAKE_CURRENT_SOURCE_DIR}/cover_db"
+        DEPENDS "${CMAKE_CURRENT_SOURCE_DIR}/cover_db"
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
     )
     add_dependencies(coverage-codecovbash symlinks)
