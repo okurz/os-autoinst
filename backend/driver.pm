7d66539ee7ea535586b0c8eaa053364e7ec993b0
Oliver Kurz <okurz@suse.de>
2021-07-30 15:17:31 +0200

    WIP -- only include when needed -- no warnings redefine

--- backend/driver.pm
+++ backend/driver.pm
@@ -23,6 +23,7 @@ use Mojo::IOLoop::ReadWriteProcess::Session 'session';
 use myjsonrpc;
 use signalblocker;
 
+no warnings 'redefine';    # unclear why this is needed
 sub new ($class, $name) {
     my $self = bless({class => $class}, $class);
 
--- bmwqemu.pm
+++ bmwqemu.pm
@@ -56,6 +56,7 @@ our @ovmf_locations = (
 our %vars;
 tie %vars, 'bmwqemu::tiedvars', %vars;
 
+no warnings 'redefine';    # unclear why this is needed
 sub result_dir { 'testresults' }
 
 sub logger { $logger //= Mojo::Log->new(level => 'debug', format => \&log_format_callback) }
